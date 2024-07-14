54b01152f2a9f6f5608eb9afbe15a2d186676519
CelestialCrafter <celestialcraftermc@gmail.com>
2024-07-13 23:21:21 -0500

    forgot to add web folder to last commit

---
+++ src/web/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import SearchResult from "./lib/SearchResult.svelte";
+
+	const fetchResults = async () => await (await fetch(import.meta.env.VITE_API_URI + "/api/search-results")).json();
+</script>
+
+<main>
+{#await fetchResults() then searchResults}
+	{#each searchResults as result}
+		<SearchResult result={result} />
+	{/each}
+{/await}
+</main>
